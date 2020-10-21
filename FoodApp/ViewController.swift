df0ba1fcbc41d0f1e7c3bd8579dff10c2100f08d
Lan Jin <lan.jin@ucalgary.ca>
2020-10-21 00:30:17 -0600

    Initial Commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Lan Jin on 2020-10-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
